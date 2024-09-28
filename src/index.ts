2606ab428d33f0568cf321396e776ebb67986ec9
Admin <benbilldev@gmail.com>
2024-09-28 14:16:50 +0700

    BE - Controller/Route: Create user route and user controller

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cors from "cors"
 import "dotenv/config"
 import { accessSync } from "fs"
 import mongoose from "mongoose"
-
+import myUserRoute from "./routes/MyUserRoute"
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
     console.log("Connected to db")
 }).catch((e) => {
@@ -15,9 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get("/", async (req: Request, res: Response) => {
-    res.json({ message: "Hello" })
-})
+app.use("/api/my/user",myUserRoute)
 
 app.listen(7000, () => {
     console.log(`Sever started on http://localhost:7000`)
