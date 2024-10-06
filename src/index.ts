a56faf3a33f099d051ebd0158d3112faab8116e1
Admin <benbilldev@gmail.com>
2024-10-06 18:03:45 +0700

    BE - Route/Controller/Middleware: Create endpoint, validation, controller to create restaurant

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import express, { Request, Response } from "express"
 import cors from "cors"
 import "dotenv/config"
-import { accessSync } from "fs"
+import { v2 as cloudinary } from "cloudinary"
 import mongoose from "mongoose"
 import myUserRoute from "./routes/MyUserRoute"
+import myRestaurantRoute from "./routes/MyRestaurantRoute"
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
     console.log("Connected to db")
 }).catch((e) => {
     console.log("Error from connection of App to DB: " + e)
 })
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 const app = express()
 
 app.use(express.json())
@@ -18,7 +24,8 @@ app.get("/health", async (req: Request, res: Response) => {
     res.send({ message: "health OK!" })
 })
 app.use("/api/my/user", myUserRoute)
+app.use("/api/my/restaurant",myRestaurantRoute)
 
 app.listen(7000, () => {
-    console.log(`Sever started on http://localhost:7000`)
+    
 })
