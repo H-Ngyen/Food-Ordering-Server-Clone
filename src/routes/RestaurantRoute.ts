0cf60914d5b367f3bed9763ab56ccf71b57265bd
Admin <benbilldev@gmail.com>
2024-10-11 08:20:43 +0700

    BE - Route/Controller: create endpoint to search restaurant by ID

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import { param } from "express-validator"
 import RestaurantController from "../controllers/RestaurantController"
+import { jwtCheck, jwtParse } from "../middleware/auth"
 const router = express.Router()
 
 // /api/restaurant/search/london
 router.get("/search/:city", param("city").isString().trim().notEmpty().withMessage("City parameter must be a valid string"),RestaurantController.searchRestaurant)
+router.get("/:restaurantId",  param("restaurantId").isString().trim().notEmpty().withMessage("Restaurant ID parameter must be a valid string"),RestaurantController.getRestaurant)
 
 export default router
