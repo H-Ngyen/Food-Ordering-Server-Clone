e9de372c8121e9cc9b796ed24e41e95b2ecab8d8
Admin <benbilldev@gmail.com>
2024-10-11 23:39:15 +0700

    BE - Controller/Route: Create checkout with Stripe for order

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import mongoose from "mongoose"
 import myUserRoute from "./routes/MyUserRoute"
 import myRestaurantRoute from "./routes/MyRestaurantRoute"
 import restaurantRoute from "./routes/RestaurantRoute"
+import orderRoute from "./routes/OrderRoute"
+import bodyParser = require("body-parser")
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
     console.log("Connected to db")
 }).catch((e) => {
@@ -18,15 +20,20 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 const app = express()
-
-app.use(express.json())
+app.use("/api/order/checkout/webhook", bodyParser.raw({ type: "*/*" }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
+
+
+
 app.get("/health", async (req: Request, res: Response) => {
     res.send({ message: "health OK!" })
 })
 app.use("/api/my/user", myUserRoute)
 app.use("/api/my/restaurant", myRestaurantRoute)
 app.use("/api/restaurant", restaurantRoute)
+app.use("/api/order", orderRoute)
 
 app.listen(7000, () => {
 
